f812d8c1a1a6341d6a7a4d1bbf0a13dc5ccf38d7
Julian Tepper <Juliantepper2006+github@gmail.com>
2025-01-02 20:55:41 +0100

    fixed lib import

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![windows_subsystem = "windows"]
 
-use auto_sort::run_sorting;
+use RustyFilesort::run_sorting;
 
 fn main() {
     let file_appender = tracing_appender::rolling::minutely("./logs", "debug.log");

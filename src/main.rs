88d37be70e5bcb71c0f634ea47f38e6686b5f75c
Julian Tepper <Juliantepper2006+github@gmail.com>
2025-01-02 23:50:54 +0100

    turned off debug files

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use rusty_filesort::run_sorting;
 
 #[tokio::main]
 async fn main() {
-    let file_appender = tracing_appender::rolling::minutely("./logs", "debug.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    tracing_subscriber::fmt().with_writer(non_blocking).init();
+    // let file_appender = tracing_appender::rolling::minutely("./logs", "debug.log");
+    // let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    // tracing_subscriber::fmt().with_writer(non_blocking).init();
 
     tracing::info!("Hello, world!");
     match run_sorting() {

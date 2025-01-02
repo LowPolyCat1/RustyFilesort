3355b8f35c00bfa9260b6d41421ac7c42e459621
Julian Tepper <Juliantepper2006+github@gmail.com>
2025-01-02 23:15:34 +0100

    updated icon path to relative path

--- build.rs
+++ build.rs
@@ -2,9 +2,7 @@ extern crate windows_exe_info;
 
 #[cfg(target_os = "windows")]
 fn main() {
-    windows_exe_info::icon::icon_ico(std::path::Path::new(
-        "D:/VSC/Rust/autosort_downloads/images/icon.ico",
-    ));
+    windows_exe_info::icon::icon_ico(std::path::Path::new("./images/icon.ico"));
 }
 
 #[cfg(not(target_os = "windows"))]
